860e5fc24dd805d0ed30238c787b0acdcd294742
Manoranjan Mishra <39005261+manoranjan76626@users.noreply.github.com>
2024-06-06 15:35:10 +0530

    Update app.js

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -4,7 +4,8 @@ app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-
+app.port(80, function () {
+}
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
